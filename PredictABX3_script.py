18850c34353d4f3c176ca60ddd69521802371c46
CJBartel <christopher.bartel@colorado.edu>
2018-01-10 13:07:55 -0700

    -a

--- PredictABX3_script.py
+++ PredictABX3_script.py
@@ -7,10 +7,12 @@ Created on Thu Nov  9 16:02:29 2017
 
 import numpy as np
 import os 
-from CompAnalyzer_script import CompAnalyzer
 import pandas as pd
 from make_radii_dict import ionic_radii_dict as Shannon_dict
 import math
+import re
+from sklearn.calibration import CalibratedClassifierCV
+
 
 class PredictABX3(object):
     """
@@ -23,51 +25,43 @@ class PredictABX3(object):
     
     def __init__(self, initial_form):
         """
-        ABX3 to get A, B, oxA, oxB, rA, rB for
+        CCX3 to classify
         """
         self.initial_form = initial_form
     
     @property
-    def ca_obj(self):
+    def els(self):
         """
-        returns CompAnalyzer object
+        list of elements in formula (str)
         """
-        return CompAnalyzer(self.initial_form)
+        return re.findall('[A-Z][a-z]?', self.initial_form)
     
     @property
-    def good_form(self):
+    def X(self):
         """
-        returns well-formatted formula
+        anion (str)
         """
-        return self.ca_obj.good_form
+        el_num_pairs = [[el_num_pair[idx] for idx in range(len(el_num_pair)) if el_num_pair[idx] != ''][0]
+                                  for el_num_pair in re.findall('([A-Z][a-z]\d*)|([A-Z]\d*)', self.initial_form)]
+        return [el_num_pair.replace('3', '') for el_num_pair in el_num_pairs if '3' in el_num_pair][0]
     
     @property
-    def atom_names(self):
+    def cations(self):
         """
-        returns alphabetical list of elements
+        list of cations (str)
         """
-        return self.ca_obj.atom_names
-    
-    @property
-    def atom_nums(self):
-        """
-        returns number of each element in compound (same order as names)
-        """
-        return self.ca_obj.atom_nums
-    
-    @property
-    def num_els(self):
-        """
-        returns number of elements in compound
-        """
-        return self.ca_obj.num_els
-    
+        els = self.els
+        return [el for el in els if el != self.X]
+        
     @property
     def X_ox_dict(self):
         """
         returns {el (str): oxidation state (int)} for allowed anions
         """
-        return {'O' : -2,
+        return {'N' : -3,
+                'O' : -2,
+                'S' : -2,
+                'Se' : -2,
                 'F' : -1,
                 'Cl' : -1,
                 'Br' : -1,
@@ -95,27 +89,20 @@ class PredictABX3(object):
         return ['Sc', 'Y', 'La', 'Al', 'Ga', 'In',
                 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb',
                 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu']    
-    
-    @property
-    def X(self):
-        """
-        returns anion (str)
-        """
-        return self.atom_names[self.atom_nums.index(3)]
-    
-    @property
-    def cations(self):
-        """
-        returns list of cations (str)
-        """
-        return [el for el in self.atom_names if el != self.X]
-    
+            
     @property
     def chi_dict(self):
         """
         returns {el (str) : Pauling electronegativity (float)} for cations
         """
-        return {cation : CompAnalyzer(cation).feat_lst('X')[0] for cation in self.cations}    
+        cations = self.cations
+        chi_dict = {}
+        with open('electronegativities.csv') as f:
+            for line in f:
+                line = line.split(',')
+                if line[0] in cations:
+                    chi_dict[line[0]] = float(line[1][:-1])
+        return chi_dict
     
     @property
     def allowed_ox(self):
@@ -126,10 +113,13 @@ class PredictABX3(object):
         cations = self.cations
         allowed_ox_dict = {}
         for cation in cations:
+            # if cation is commonly 1+, make that the only allowed oxidation state
             if cation in self.plus_one:
                 allowed_ox_dict[cation] = [1]
+            # if cation is commonly 2+, make that the only allowed oxidation state
             elif cation in self.plus_two:
                 allowed_ox_dict[cation] = [2]
+            # otherwise, use the oxidation states that have corresponding Shannon radii                
             else:
                 allowed_ox_dict[cation] = [val for val in list(Shannon_dict[cation].keys()) if val > 0]
         allowed_ox_dict[X] = [self.X_ox_dict[X]]
@@ -159,60 +149,64 @@ class PredictABX3(object):
             return bal_combos 
     
     @property
-    def choose_combo(self):
+    def chosen_ox_states(self):
         """
         returns {el (str) : assigned oxidation state (int)} for cations
         """
         combos = self.charge_bal_combos
-        chi_lst = list(self.chi_dict.values())
+        chi_dict = self.chi_dict
         cations = self.cations
         X = self.X
-        if isinstance(combos, float):
-            return np.nan
-        AB_lst = cations
+        plus_three =self.plus_three
+        # if only one charge-balanced combination exists, use it
         if len(combos) == 1:
-            return dict(zip(AB_lst, list(combos[0])))
+            ox_states = dict(zip(cations, combos[0]))
+        # if two combos exists and they are the reverse of one another
         elif (len(combos) == 2) and (combos[0] == combos[1][::-1]):
+            # assign the minimum oxidation state to the more electronegative cation
             min_ox = np.min(combos[0])
-            max_ox = np.max(combos[0])
-            epos_idx = chi_lst.index(np.min(chi_lst))
-            eneg_idx = chi_lst.index(np.max(chi_lst))
-            return {AB_lst[epos_idx] : max_ox,
-                    AB_lst[eneg_idx] : min_ox}
+            max_ox = np.max(combos[1])
+            epos_el = [el for el in cations if chi_dict[el] == np.min(list(chi_dict.values()))][0]
+            eneg_el = [el for el in cations if el != epos_el][0]
+            ox_states = {epos_el : max_ox,
+                         eneg_el : min_ox}
         else:
-            if (AB_lst[0] in self.plus_three) or (AB_lst[1] in self.plus_three):
+            # if one of the cations is probably 3+, let it be 3+
+            if (cations[0] in plus_three) or (cations[1] in plus_three):
                 if X == 'O':
                     if (3,3) in combos:
                         combo = (3,3)
-                        return dict(zip(AB_lst, list(combo)))
-            elif np.min(chi_lst) > 0.9 * np.max(chi_lst):
+                        ox_states = dict(zip(cations, list(combo)))
+            # else compare electronegativities - if 0.9 < chi1/chi2 < 1.1, minimize the oxidation state diff
+            elif np.min(list(chi_dict.values())) > 0.9 * np.max(list(chi_dict.values())):
                 diffs = [abs(combo[0] - combo[1]) for combo in combos]
                 mindex = [idx for idx in range(len(diffs)) if diffs[idx] == np.min(diffs)]
                 if len(mindex) == 1:
                     mindex = mindex[0]
                     combo = combos[mindex]
+                    ox_states = dict(zip(cations, combo))
                 else:
                     min_ox = np.min([combos[idx] for idx in mindex])
                     max_ox = np.max([combos[idx] for idx in mindex])
-                    epos_idx = chi_lst.index(np.min(chi_lst))
-                    eneg_idx = chi_lst.index(np.max(chi_lst))
-                    return {AB_lst[epos_idx] : max_ox,
-                            AB_lst[eneg_idx] : min_ox}
-                return dict(zip(AB_lst, list(combos[mindex])))
+                    epos_el = [el for el in cations if chi_dict[el] == np.min(list(chi_dict.values()))][0]
+                    eneg_el = [el for el in cations if el != epos_el][0]
+                    ox_states = {epos_el : max_ox,
+                                 eneg_el : min_ox} 
             else:
                 diffs = [abs(combo[0] - combo[1]) for combo in combos]
                 maxdex = [idx for idx in range(len(diffs)) if diffs[idx] == np.max(diffs)]
                 if len(maxdex) == 1:
                     maxdex = maxdex[0]
                     combo = combos[maxdex]
+                    ox_states = dict(zip(cations, combo))
                 else:
                     min_ox = np.min([combos[idx] for idx in maxdex])
                     max_ox = np.max([combos[idx] for idx in maxdex])
-                    epos_idx = chi_lst.index(np.min(chi_lst))
-                    eneg_idx = chi_lst.index(np.max(chi_lst))
-                    return {AB_lst[epos_idx] : max_ox,
-                            AB_lst[eneg_idx] : min_ox}
-                return dict(zip(AB_lst, list(combos[maxdex])))
+                    epos_el = [el for el in cations if chi_dict[el] == np.min(list(chi_dict.values()))][0]
+                    eneg_el = [el for el in cations if el != epos_el][0]
+                    ox_states = {epos_el : max_ox,
+                                eneg_el : min_ox}
+        return ox_states
     
     @property
     def AB_radii_dict(self):
@@ -220,20 +214,24 @@ class PredictABX3(object):
         returns {el (str) : {'A_rad' : radius if A (float),
                              'B_rad' : radius if B (float)}}
         """
-        ox_dict = self.choose_combo
+        ox_dict = self.chosen_ox_states
         if isinstance(ox_dict, float):
             return np.nan
         radii_dict = {}
         for el in ox_dict:
             tmp_dict = {}
+            # get the oxidation state
             ox = ox_dict[el]
             coords = list(Shannon_dict[el][ox].keys())
+            # get the B CN as the one available nearest 6
             B_coords = [abs(coord - 6) for coord in coords]
             mindex = [idx for idx in range(len(B_coords)) if B_coords[idx] == np.min(B_coords)][0]
             B_coord = coords[mindex]
+            # get the A CN as the one available nearest 12
             A_coords = [abs(coord - 12) for coord in coords]
             mindex = [idx for idx in range(len(A_coords)) if A_coords[idx] == np.min(A_coords)][0]
             A_coord = coords[mindex]
+            # produce the equivalent B-site and A-site radii
             B_rad = Shannon_dict[el][ox][B_coord]['only_spin']
             A_rad = Shannon_dict[el][ox][A_coord]['only_spin']
             tmp_dict['A_rad'] = A_rad
@@ -246,7 +244,7 @@ class PredictABX3(object):
         """
         returns predicted A (str)
         """
-        ox_dict = self.choose_combo
+        ox_dict = self.chosen_ox_states
         if isinstance(ox_dict, float):
             return np.nan        
         radii_dict = self.AB_radii_dict
@@ -291,20 +289,20 @@ class PredictABX3(object):
         """
         returns oxidation state assigned to A (int)
         """
-        if isinstance(self.choose_combo, float):
+        if isinstance(self.chosen_ox_states, float):
             return np.nan
         else:
-            return self.choose_combo[self.pred_A]
+            return self.chosen_ox_states[self.pred_A]
     
     @property
     def nB(self):
         """
         returns oxidation state assigned to B (int)
         """
-        if isinstance(self.choose_combo, float):
+        if isinstance(self.chosen_ox_states, float):
             return np.nan
         else:        
-            return self.choose_combo[self.pred_B]
+            return self.chosen_ox_states[self.pred_B]
     
     @property
     def rA(self):
@@ -355,6 +353,9 @@ class PredictABX3(object):
         
     @property
     def tau(self):
+        """
+        returns tau (float)
+        """
         if isinstance(self.AB_radii_dict, float):
             return np.nan
         else:
@@ -364,55 +365,74 @@ class PredictABX3(object):
                 return np.nan
     
     @property
-    def is_perov(self):
+    def tau_pred(self):
+        """
+        returns prediction of 1 (perovskite) or -1 (nonperovskite) based on tau
+        """
         if math.isnan(self.tau):
             return np.nan
         else:
             return [1 if self.tau < 4.18 else -1][0]
         
-
     @property
-    def inv_rA(self):
-        if isinstance(self.AB_radii_dict, float):
-            return np.nan
-        else:        
-            return self.AB_radii_dict[self.pred_A]['B_rad']
-        
-    @property
-    def inv_rB(self):
-        if isinstance(self.AB_radii_dict, float):
+    def t_pred(self):
+        """
+        returns prediction of 1 (perovskite) or -1 (nonperovskite) based on t
+        """        
+        if math.isnan(self.t):
             return np.nan
         else:
-            return self.AB_radii_dict[self.pred_B]['A_rad']
+            return [1 if (self.t > 0.825) and (self.t < 1.059) else -1][0]  
+        
+    @property
+    def calibrate_tau(self):
+        """
+        returns a calibrated classifier to yield tau probabilities
+        """
+        df = pd.read_csv('TableS1.csv')
+        df['tau'] = [PredictABX3(ABX3).tau for ABX3 in df.ABX3.values]
+        X, y = df['tau'].values.reshape(-1, 1), df['exp_label'].values
+        clf = CalibratedClassifierCV(cv=3)
+        clf.fit(X, y)
+        return clf
     
+    def tau_prob(self, clf):
+        """
+        Args:
+            clf (sklearn object) - calibrated classifier based on tau
+        Returns:
+            probability of perovskite based on tau (float)
+        """
+        X = [[self.tau]]
+        return clf.predict_proba(X)[0][1]
+    
+            
 def main():
-    print('-----THIS IS A DEMONSTRATION-----')
-    test_formula = 'LaGaO3'
-    obj = PredictABX3(test_formula)
-    good_form = obj.good_form
-    A = obj.pred_A
-    B = obj.pred_B
-    oxA = obj.nA
-    oxB = obj.nB
-    rA = obj.rA
-    rB = obj.rB
-    mu = obj.mu
-    t = obj.t
-    tau = obj.tau
-    is_perov = obj.is_perov
     
-    print('The formula for demonstration is %s' % test_formula)
-    print('A is predicted to be %s' % A)
-    print('B is predicted to be %s' % B)
-    print('The oxidation state of A is predicted to be %s' % oxA)
-    print('The oxidation state of B is predicted to be %s' % oxB)
-    print('The radius of A is predicted to be %.2f A' % rA)
-    print('The radius of B is predicted to be %.2f A' % rB)
-    print('which gives a Goldschmidt factor of %.3f' % t)
-    print('and an octahedral factor of %.3f' % mu)
+    # read in TableS1.csv which contains our experimental dataset and associated information
+    df = pd.read_csv('TableS1.csv')
+    # generate t using the current script
+    df['t_check'] = [PredictABX3(ABX3).t for ABX3 in df.ABX3.values]
+    # compare t generated here to t in TableS1.csv
+    df['t_diff'] = abs(df['t'] - df['t_check'])
+    print('The max difference for t = %.3f' % df.t_diff.max())
     
-    return obj
+    # generate tau using the current script
+    df['tau_check'] = [PredictABX3(ABX3).tau for ABX3 in df.ABX3.values]
+    # compare tau generated here to tau in TableS1.csv
+    df['tau_diff'] = abs(df['tau'] - df['tau_check'])
+    print('The max difference for tau = %.3f' % df.tau_diff.max())
+    
+    # fit calibrated classifier for tau
+    clf = PredictABX3('').calibrate_tau
+    
+    # generate tau  probabilities using the current script
+    df['tau_prob_check'] = [PredictABX3(ABX3).tau_prob(clf) for ABX3 in df.ABX3.values]
+    # compare tau probabilities generated here to those in TableS1.csv
+    df['tau_prob_diff'] = abs(df['tau_prob'] - df['tau_prob_check'])
+    print('The max difference for tau probabilities = %.3f' % df.tau_prob_diff.max())
+    return df
     
 if __name__ == '__main__':
-    obj = main()
+    df = main()
     
